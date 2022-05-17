900c46f14514bcb506cea8c6d0cf095e2b49a14d
Nathan <90482492+Nathanmjk@users.noreply.github.com>
2022-05-17 18:16:06 -0300

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -1,21 +1,16 @@
-function validando(){
-    let value = document.getElementById("emailID").value;
-    let valueSENHA = document.getElementById("senhaID").value;
+function acessar() {
+  var dados = $("#form").serialize();
 
-    let regexEmail =  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    let regexSenha = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-    
-    if (!regexEmail.test(value)) {
-      // campo inválido, retorna false para o formulário não ser submetido
-      alert('Email Inválido');
-      return false;
+  $.ajax({
+    type: "POST",
+    data: dados,
+    url: "../php/acessar.php",
+    success: function(retorno){
+      window.location.href = "../paginas/Enviar token.html";
+    },
+    error: function(){
+      alert("Ocorreu um erro");
     }
-
-    else if(!regexSenha.test(valueSENHA)) {
-        alert("SENHA Invalida");
-        return false;
-    }
-
-    return true;
-
+  });
+  
 }
